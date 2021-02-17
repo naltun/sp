eab03c52190217c230b038306025a1e18c3d110e
Noah Altunian <nbaltunian@gmail.com>
2021-02-17 15:26:35 -0500

    Add port scanning using netcat

--- sp.sh
+++ sp.sh
@@ -13,8 +13,24 @@ check_args() {
     [ -z "$2" ] && help && exit 2
 }
 
+run() {
+    case "$2" in
+        *-*)
+            start_port=$(echo "$2" | awk '{split($0, ports, "-"); print ports[1]}')
+            end_port=$(echo "$2"   | awk '{split($0, ports, "-"); print ports[2]}')
+            for port in $(seq "$start_port" "$end_port"); do
+                nc -z "$1" "$port"
+            done
+            ;;
+        *)
+            nc -z "$1" "$2"
+            ;;
+    esac
+}
+
 main() {
     check_args "$@"
+    run "$@"
 }
 
 main "$@"
