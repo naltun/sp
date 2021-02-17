873d68d8eb70a9af44a2a4279f6df71e55aae2b2
Noah Altunian <nbaltunian@gmail.com>
2021-02-17 14:51:45 -0500

    Refactor

--- sp.sh
+++ sp.sh
@@ -1,17 +1,20 @@
+#!/bin/sh
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-#!/bin/sh
 
 help() {
     echo "USAGE: sp [hostname] [port[s]]"
     echo "Eg. sp scanme.nmap.org 22-100"
 }
 
-[ -z "$1" ] \
-    && help \
-    && exit 1
+check_args() {
+    [ -z "$1" ] && help && exit 1
+    [ -z "$2" ] && help && exit 2
+}
 
-[ -z "$2" ] \
-    && help \
-    && exit 2
+main() {
+    check_args "$@"
+}
+
+main "$@"
